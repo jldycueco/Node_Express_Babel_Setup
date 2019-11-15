5f33ae5a58a28320da12a47de3c77ba22bbbcc54
JD <laeim017@gmail.com>
2019-11-15 16:09:29 +0800

    updated scripts

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
-import express from "express";
-import "dotenv/config";
-import cors from "cors";
+import express from 'express';
+import 'dotenv/config';
+import cors from 'cors';
 
 const app = express();
 app.use(cors());
