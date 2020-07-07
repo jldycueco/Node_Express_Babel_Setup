12bfb45bee5af5abf03adc402db93c03fcbba980
Jonathan Darwin Dycueco <jonathandarwindycueco@TLLCMs-MacBook-Air.local>
2020-07-07 16:54:50 +0800

    add eslint, update code

--- src/server.js
+++ src/server.js
@@ -1,9 +1,11 @@
 import express from 'express';
-import 'dotenv/config';
 import cors from 'cors';
+import config from './config';
+
+const { PORT } = config;
 
 const app = express();
 app.use(cors());
 
-const port = process.env.PORT || 5000;
-app.listen(port, () => console.log(`Server running on port ${port}`));
+// eslint-disable-next-line no-console
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
