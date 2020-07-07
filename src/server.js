a262db981bbe1a537b9e1650ca4274d0fb047ca7
Jonathan Darwin Dycueco <jonathandarwindycueco@TLLCMs-MacBook-Air.local>
2020-07-07 16:56:17 +0800

    add route

--- src/server.js
+++ src/server.js
@@ -1,11 +1,15 @@
 import express from 'express';
 import cors from 'cors';
 import config from './config';
+import indexRoute from './route/indexRoute';
 
 const { PORT } = config;
 
 const app = express();
 app.use(cors());
+
+// Router
+app.use('/', indexRoute);
 
 // eslint-disable-next-line no-console
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
